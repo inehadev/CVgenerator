db9e38cc5e3d5cda3e6e08c67b96383ded4113e8
inehadev <n988420@gmail.com>
2024-11-10 17:38:16 +0530

    solved some deployment error

--- src/app/CVform/page.jsx
+++ src/app/CVform/page.jsx
@@ -68,7 +68,7 @@ const CVForm = () => {
 
     try {
        
-        const response = await axios.post('http://localhost:3000/api/generateCV', bodyParameter, axiosheader);
+        const response = await axios.post('https://c-vgenerator-one.vercel.app', bodyParameter, axiosheader);
         
         console.log('Response :', response.data);
         
